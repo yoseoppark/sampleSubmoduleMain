d07d6657ce82b1a33a46b83289d26f0f13c1d90d
yoseop park <yoseop.park@hyundai.com>
2021-07-14 11:28:15 +0900

    서브모듈 제거.

---
+++ samplemodule1/SampleModule1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleModule1
+//
+//  Created by YoseopPark on 2021/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
